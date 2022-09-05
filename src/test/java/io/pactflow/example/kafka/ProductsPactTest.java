69fb565bcfd571d235c34329e7f30b35656b75dc
Кадников Антон <kadnikov.ant@gmail.com>
2022-09-05 10:25:36 +0300

    .

--- src/test/java/io/pactflow/example/kafka/ProductsPactTest.java
+++ src/test/java/io/pactflow/example/kafka/ProductsPactTest.java
@@ -1,7 +1,8 @@
 package io.pactflow.example.kafka;
 
 import au.com.dius.pact.core.model.PactSpecVersion;
-import org.assertj.core.api.Condition;
+import au.com.dius.pact.core.model.V4Interaction;
+import au.com.dius.pact.core.model.V4Pact;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,18 +12,8 @@ import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.consumer.junit5.ProviderType;
-import au.com.dius.pact.core.model.messaging.Message;
-import au.com.dius.pact.core.model.messaging.MessagePact;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @ExtendWith(PactConsumerTestExt.class)
@@ -33,7 +24,7 @@ public class ProductsPactTest {
     ProductEventListener listener;
 
     @Pact(consumer = "pactflow-example-consumer-java-kafka")
-    MessagePact createPact(MessagePactBuilder builder) {
+    V4Pact createPact(MessagePactBuilder builder) {
         PactDslJsonBody body = new PactDslJsonBody();
         body.stringType("name", "product name");
         body.stringType("type", "product series");
@@ -41,27 +32,22 @@ public class ProductsPactTest {
         body.stringMatcher("version", "v[a-zA-z0-9]+", "v1");
         body.stringMatcher("event", "^(CREATED|UPDATED|DELETED)$", "CREATED");
 
-//    Map<String, Object> metadata = new HashMap<>();
-//    metadata.put("Content-Type", "application/json");
-//    metadata.put("kafka_topic", "products");
-
         return builder.expectsToReceive("a product created event").withMetadata(md -> {
             md.add("Content-Type", "application/json");
             md.add("kafka_topic", "products");
-            md.matchRegex("Content-Type","application/json","application/json");
+            md.add("test", "test");
         }).withContent(body).toPact();
     }
 
     @Test
-    @PactTestFor(pactMethod = "createPact", providerType = ProviderType.ASYNCH, pactVersion = PactSpecVersion.V3)
-    void test(List<Message> messages) throws Exception {
+    @PactTestFor(pactMethod = "createPact", providerType = ProviderType.ASYNCH, pactVersion = PactSpecVersion.V4)
+    void test(V4Interaction.AsynchronousMessage message) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        System.out.println("Message received -> " + messages.get(0).contentsAsString());
-        Product product = mapper.readValue(messages.get(0).contentsAsString(), Product.class);
+        System.out.println("Message received -> " + message.getContents().getContents().valueAsString());
+        Product product = mapper.readValue(message.getContents().getContents().valueAsString(), Product.class);
 
         assertDoesNotThrow(() -> {
             listener.listen(product);
         });
-        assertThat(messages.get(0).getMetadata()).containsEntry("contentType", "application/json");
     }
 }
